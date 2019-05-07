2f27cb4146a275b4d9229d3f23d298b287b77785
Claudwin <claudwin.fortune@gmail.com>
2019-05-07 12:50:06 -0400

    string manipulation

--- strings.rb
+++ strings.rb
@@ -1,3 +1,14 @@
-name = "Claude"
+puts "Name an animal"
+animal = gets.chomp
 
-puts name
+puts "Name is a noun"
+noun = gets.chomp
+
+p "ths quick brown #{animal} jumped over the lazy #{noun} "  
+
+# for string interpelation you use double quotes
+# single quotes makes everything inside a string
+# A interpolation bracket can be used to run any ruby code
+
+"Astros".reverse.upcase #string manipulation
+
