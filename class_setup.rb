7e4be447eca12cb96511569e44152a66cc00cbbd
Claudwin <claudwin.fortune@gmail.com>
2019-05-07 13:31:56 -0400

    initialize class

--- class_setup.rb
+++ class_setup.rb
@@ -2,11 +2,13 @@ class ApiConnector
   #attributes "varibale for this class"
   attr_acceessor :title, :description, :url
 
-  def test_method
-    puts "testing class call"
+  def initialize (title: title, description: description, url: url = "google.com")
+    # will run when ever you create a new instance of the class
+    @title = title
+    @description = description
+    @url = url
   end
 end
 
 api = ApiConnector.new
 
-api.test_method
